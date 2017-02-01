f25cc9043deac4c1f5b415ac11755a2068aad4f6
MasonAWing <wingma071700@cvh.rusd.us>
2017-02-01 11:31:44 -0800

    turtle 2

--- Turtle.py
+++ Turtle.py
@@ -1,8 +1,10 @@
 """Create a Turtle Program that will draw a 3-dimensional cube"""
-
-
+import turtle
+ben = turtle.Turtle
 
 
 
 """Import and Call the DrawRectangle(Anyturtle, l, w) function from the
 file MyFile.py"""
+from myexamfile.py import DrawRectangle 
+DrawRectangle(ben, 15, 30)
