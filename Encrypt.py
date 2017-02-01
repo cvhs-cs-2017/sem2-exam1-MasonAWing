2dfb966c58e12495296e96b80d610b2d5a1679e8
MasonAWing <wingma071700@cvh.rusd.us>
2017-02-01 10:42:27 -0800

    encrypt 1 problem done should not have said diggity

--- Encrypt.py
+++ Encrypt.py
@@ -3,11 +3,15 @@
 'Computer Science Makes the World go round but it doesn't make the world round itself!'
 
 Print the save the result as the variable = NoVowels
-""""
-
-
-
-
-
+"""
+def novowels(s):
+    vowels = "aeiouAEIOU"
+    word = ""
+    for x in s:
+        if x in vowels:
+            x = ""
+        word += x
+    return word
+print(novowels('Computer Science Makes the World go round but it doesn\'t make the world round itself!'))
 
 """Write an encryption code that you make up and run it for the variable NoVowels"""
