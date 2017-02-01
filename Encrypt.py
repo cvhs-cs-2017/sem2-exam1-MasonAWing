6fbdeb59c9e65cdc32d88c1cc9fce2ec2b7a0198
MasonAWing <wingma071700@cvh.rusd.us>
2017-02-01 11:27:40 -0800

    encrypt 2

--- Encrypt.py
+++ Encrypt.py
@@ -4,6 +4,7 @@
 
 Print the save the result as the variable = NoVowels
 """
+import random
 def novowels(s):
     vowels = "aeiouAEIOU"
     word = ""
@@ -15,3 +16,25 @@ def novowels(s):
 print(novowels('Computer Science Makes the World go round but it doesn\'t make the world round itself!'))
 
 """Write an encryption code that you make up and run it for the variable NoVowels"""
+def chaos(s):
+    consonants = "bcdfghjklmnpqrstvxzBCDFGHJKLMNPQRSTVXZ"
+    word = ""
+    for x in s:
+        for x in s:
+            if x in consonants:
+                x = random.randint(1, 6)
+                if x == 1:
+                    x = "$"
+                if x == 2:
+                    x = "#"
+                if x == 3:
+                    x = "%"
+                if x == 4:
+                    x = "^"
+                if x == 5:
+                    x = "&"
+                if x == 6:
+                    x = "*"
+        word += x
+    return word
+print(chaos(novowels('Computer Science Makes the World go round but it doesn\'t make the world round itself')))
