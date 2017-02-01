34741032c131552a6e86d16955aafda3f576f320
MasonAWing <wingma071700@cvh.rusd.us>
2017-02-01 11:12:02 -0800

    1 range

--- RangeFunction.py
+++ RangeFunction.py
@@ -1,10 +1,13 @@
 """Use the range function to print the numbers from 40 to 1 (backwards)"""
 
-
+for x in range(1, 41):
+    while x > 0:
+        print(x)
+        x = x - 1
 
 """Repeat the exercise but count by 5's"""
 
 
 
-"""Write a program that will count print all the multiples of (n) where n is
+"""Write a program that will count print first 10 the multiples of (n) where n is
 taken from user input.  Include necessary print statements."""
