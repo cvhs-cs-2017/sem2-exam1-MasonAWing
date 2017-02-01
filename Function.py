e268862027e64c8c3f6014dd80c679c265118495
MasonAWing <wingma071700@cvh.rusd.us>
2017-02-01 11:40:25 -0800

    function 2

--- Function.py
+++ Function.py
@@ -3,10 +3,13 @@ the result"""
 def trip(n):
     n = n * 3
     return n
-print(trip(3))
+#print(trip(3))
 
 
 
 """Write a program that will prompt the user for an input value (n) and print
 the result of 3n by calling the function defined above.  Make sure you include
 the necessary print statements and address any issues with whitespace. """
+print('put a numbah')
+n = int(input())
+print(trip(n))
