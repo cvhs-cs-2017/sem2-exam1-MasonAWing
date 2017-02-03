f9d15a899c0b83dcd516702b5cd0999cd1675cb2
MasonAWing <wingma071700@cvh.rusd.us>
2017-02-03 10:37:51 -0800

    cube done

--- Turtle.py
+++ Turtle.py
@@ -1,10 +1,37 @@
 """Create a Turtle Program that will draw a 3-dimensional cube"""
 import turtle
-ben = turtle.Turtle
-
-
-
+ben = turtle.Turtle()
+for i in range(4):
+    ben.forward(50)
+    ben.right(90)
+ben.right(330)
+ben.forward(50)
+ben.backward(50)
+ben.right(30)
+ben.forward(50)
+ben.right(330)
+ben.forward(50)
+ben.backward(50)
+ben.right(120)
+ben.forward(50)
+ben.right(240)
+ben.forward(50)
+ben.backward(50)
+ben.right(210)
+ben.forward(50)
+ben.right(150)
+ben.forward(50)
+ben.backward(50)
+ben.right(300)
+ben.forward(50)
+ben.right(60)
+ben.forward(50)
+ben.right(30)
+for i in range(4):
+    ben.forward(50)
+    ben.right(90)
+input()
 """Import and Call the DrawRectangle(Anyturtle, l, w) function from the
 file MyFile.py"""
-from myexamfile.py import DrawRectangle 
+from myexamfile.py import DrawRectangle
 DrawRectangle(ben, 15, 30)
