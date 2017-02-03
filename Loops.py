f4905bab2f46f34af71e08a40a52e88c4aa7b363
MasonAWing <wingma071700@cvh.rusd.us>
2017-02-03 10:47:51 -0800

    loops 1

--- Loops.py
+++ Loops.py
@@ -2,11 +2,14 @@
 than 100.  It should then spit out the result AND tell the user how many times
 it had to add 5 (if any)"""
 
-
-
-
-
-
+def tired(n):
+    m = 0
+    while n < 100:
+        n = n + 5
+        m = m + 1
+    print(n)
+    print(m)
+tired(5)
 """Write a program that will prompt the user for an input value (n) and double
 it IF is an ODD number, triple it if is an EVEN number and do nothing if it is
 anything else (like a decimal or a string)"""
